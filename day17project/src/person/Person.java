d0ca1deb3f93f76ec5f33f8a5d31dc38c184b49c
Nikola Knezevic <knezevic97kg@gmail.com>
2022-02-25 10:43:58 +0100

    update_file

---
+++ day17project/src/person/Person.java
@@ -0,0 +1,18 @@
+package person;
+
+public class Person {
+
+	String name;
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+	
+	
+}
